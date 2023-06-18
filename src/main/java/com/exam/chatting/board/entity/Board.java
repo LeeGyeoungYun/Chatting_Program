2922abf7ea92315d7c08295c8ec58b9c9f751ff4
GYEOUNG YUN LEE <wodbsdl17@naver.com>
2023-06-18 23:55:29 +0900

    어노테이션 부연설명

--- src/main/java/com/exam/chatting/board/entity/Board.java
+++ src/main/java/com/exam/chatting/board/entity/Board.java
@@ -12,7 +12,20 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-@Getter
+/*
+ * @Getter 은 해당 클래스에 포함된 멤버변수의 모든 getter 메서드를 생성해주는 lombok의 강력한 기능입니다.
+ * 
+ * @NoArgsConstructor(access = AccessLevel.PROTECTED)은 해당 클래스의 기본 생성자를 생성해주는 어노테이션입니다.
+ * 	access속성을 이용해서 동일한 패키지내의 클래스에서만 객체를 생성할 수 있도록 제어합니다.
+ * 	
+ *	@Entity는 해당 클래스가 테이블과 매핑되는 JPA의 엔티티(Entity) 클래스임을 의미합니다.
+ *   기본적으로 클래스명을 테이블명으로 매핑합니다. 예를들어 user_role이라는 테이블은 UserRole이라는 이름의 클래스로 네이밍 하면되고, 
+ *   혹시라도 클래스명과 테이블명이 다를 수 밖에 없는 상황에서는 클래스 레벨에 @Table을 선언하고, @Table(name ="user_role") 과 같이 
+ *   name 속성을 이용해서 처리해 주면 됩니다.
+ *   
+ * */
+
+@Getter   
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Entity
 public class Board {
